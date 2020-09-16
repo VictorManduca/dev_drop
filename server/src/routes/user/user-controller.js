b5e6bc2ee675a665efdbd600c4c3d83f54d7d670
VictorManduca <victor.manduca@hotmail.com>
2020-09-16 01:25:54 -0300

    User's crud

--- server/src/routes/user/user-controller.js
+++ server/src/routes/user/user-controller.js
@@ -6,18 +6,71 @@ import models from '../../models'
 import userSchema from '../schemas/user-schema.json'
 
 export async function create(req, res, next) {
-	const body = { ...req.body }
+	try {
+		const body = { ...req.body }
 
-	const validator = validate(userSchema, body)
-	if (validator !== true)
-		return callback.badRequest(res, validator)
+		const validator = validate(userSchema, body)
+		if (validator !== true)
+			return callback.badRequest(res, validator)
 
-	const user = await models.usuario.create(body)
+		const user = await models.usuario.create(body)
 
-	return callback.created(res, user.dataValues.id)
+		return callback.created(res, user.dataValues.id)
+	} catch (error) {
+		return callback.badRequest(res, error.message)
+	}
 }
 
 export async function all(req, res, next) {
-	const users = await models.usuario.findAll()
-	return callback.withData(res, users.dataValues)
+	try {
+		const users = await models.usuario.findAll()
+
+		if (users.length == 0)
+			return callback.withData(res, [])
+
+		return callback.withData(res, users)
+	} catch (error) {
+		return callback.badRequest(res, error.message)
+	}
+}
+
+export async function one(req, res, next) {
+	try {
+		const { id } = req.params
+		const user = await models.usuario.findOne({ where: { id: id } })
+
+		if (!user.dataValues)
+			return callback.withData(res, [])
+
+		return callback.withData(res, user.dataValues)
+	} catch (error) {
+		return callback.badRequest(res, error.message)
+	}
+}
+
+export async function update(req, res, next) {
+	try {
+		const { id } = req.params
+		const body = { ...req.body }
+
+		const validator = validate(userSchema, body)
+		if (validator !== true)
+			return callback.badRequest(res, validator)
+
+		await models.usuario.update(body, { where: { id: id } })
+		return callback.emptyOk(res)
+	} catch (error) {
+		return callback.badRequest(res, error.message)
+	}
+}
+
+export async function destroy(req, res, next) {
+	try {
+		const { id } = req.params
+
+		await models.usuario.destroy({ where: { id: id } })
+		return callback.emptyOk(res)
+	} catch (error) {
+		return callback.badRequest(res, error.message)
+	}
 }
