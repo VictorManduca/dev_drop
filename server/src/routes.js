5b2f2420e30c5574ae677cf2d9a8e9663d06cf55
VictorManduca <victor.manduca@hotmail.com>
2020-09-16 01:24:15 -0300

    Adding new routes to user

--- server/src/routes.js
+++ server/src/routes.js
@@ -6,5 +6,8 @@ const router = express.Router()
 
 router.post('/drop/user', user.create)
 router.get('/drop/user', user.all)
+router.get('/drop/user/:id', user.one)
+router.put('/drop/user/:id', user.update)
+router.delete('/drop/user/:id', user.destroy)
 
 export default router
