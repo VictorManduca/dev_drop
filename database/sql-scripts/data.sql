0c09c5191e5d1aca0c012a7b3f906a62a5e0996c
Victor Manduca <victor.manduca@hotmail.com>
2020-10-08 15:25:44 -0300

    Creating structure for database in docker hub

--- database/sql-scripts/data.sql
+++ database/sql-scripts/data.sql
@@ -1,25 +1,24 @@
--- MySQL dump 10.13  Distrib 8.0.20, for Linux (x86_64)
+-- MySQL dump 10.17  Distrib 10.3.22-MariaDB, for debian-linux-gnu (x86_64)
 --
--- Host: 127.0.0.1    Database: DropDB
+-- Host: 127.0.0.1    Database: devDropDB
 -- ------------------------------------------------------
--- Server version	8.0.21
+-- Server version	10.5.5-MariaDB-1:10.5.5+maria~focal
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Current Database: `DropDB`
+-- Current Database: `devDropDB`
 --
 
-USE `DropDB`;
+USE `devDropDB`;
 
 --
 -- Dumping data for table `arquivos`
@@ -31,21 +30,30 @@ LOCK TABLES `arquivos` WRITE;
 UNLOCK TABLES;
 
 --
--- Dumping data for table `categoria`
+-- Dumping data for table `grupo`
 --
 
-LOCK TABLES `categoria` WRITE;
-/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
-/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
+LOCK TABLES `grupo` WRITE;
+/*!40000 ALTER TABLE `grupo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `grupo` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Dumping data for table `planos`
+-- Dumping data for table `pasta`
 --
 
-LOCK TABLES `planos` WRITE;
-/*!40000 ALTER TABLE `planos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `planos` ENABLE KEYS */;
+LOCK TABLES `pasta` WRITE;
+/*!40000 ALTER TABLE `pasta` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pasta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `permissao`
+--
+
+LOCK TABLES `permissao` WRITE;
+/*!40000 ALTER TABLE `permissao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `permissao` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -65,32 +73,13 @@ LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping data for table `vinculo_planos_usuario`
---
-
-LOCK TABLES `vinculo_planos_usuario` WRITE;
-/*!40000 ALTER TABLE `vinculo_planos_usuario` DISABLE KEYS */;
-/*!40000 ALTER TABLE `vinculo_planos_usuario` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping data for table `vinculo_usuario_pagamento`
---
-
-LOCK TABLES `vinculo_usuario_pagamento` WRITE;
-/*!40000 ALTER TABLE `vinculo_usuario_pagamento` DISABLE KEYS */;
-/*!40000 ALTER TABLE `vinculo_usuario_pagamento` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-08-14 20:34:38
+-- Dump completed on 2020-10-08 15:09:10
--- database/sql-scripts/structure.sql
+++ database/sql-scripts/structure.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.20, for Linux (x86_64)
+-- MySQL dump 10.17  Distrib 10.3.22-MariaDB, for debian-linux-gnu (x86_64)
 --
--- Host: 127.0.0.1    Database: DropDB
+-- Host: 127.0.0.1    Database: devDropDB
 -- ------------------------------------------------------
--- Server version	8.0.21
+-- Server version	10.5.5-MariaDB-1:10.5.5+maria~focal
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -15,8 +15,8 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE DATABASE `DropDB`;
-USE `DropDB`;
+CREATE DATABASE devDropDB;
+USE devDropDB;
 
 --
 -- Table structure for table `arquivos`
@@ -24,53 +24,81 @@ USE `DropDB`;
 
 DROP TABLE IF EXISTS `arquivos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `arquivos` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `UsuarioID` int DEFAULT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `UsuarioID` int(11) DEFAULT NULL,
   `Arquivo` varchar(255) DEFAULT NULL,
-  `eFavorito` tinyint DEFAULT NULL,
+  `eFavorito` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`ID`),
   KEY `arquivos` (`UsuarioID`),
-  CONSTRAINT `arquivos_ibfk_1` FOREIGN KEY (`UsuarioID`) REFERENCES `usuario` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  CONSTRAINT `arquivos` FOREIGN KEY (`UsuarioID`) REFERENCES `usuario` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `categoria`
+-- Table structure for table `grupo`
 --
 
-DROP TABLE IF EXISTS `categoria`;
+DROP TABLE IF EXISTS `grupo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
-CREATE TABLE `categoria` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `ArquivoID` int DEFAULT NULL,
-  `UsuarioID` int DEFAULT NULL,
-  `TipoCategoriaID` int DEFAULT NULL,
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `grupo` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `UsuarioID` int(11) DEFAULT NULL,
+  `PastaID` int(11) DEFAULT NULL,
+  `TipoCategoriaID` int(11) DEFAULT NULL,
+  `ArquivoID` int(11) DEFAULT NULL,
   PRIMARY KEY (`ID`),
-  KEY `ArquivoID` (`ArquivoID`),
-  KEY `UsuarioID` (`UsuarioID`),
-  KEY `TipoCategoriaID` (`TipoCategoriaID`),
-  CONSTRAINT `categoria_ibfk_1` FOREIGN KEY (`ArquivoID`) REFERENCES `arquivos` (`ID`),
-  CONSTRAINT `categoria_ibfk_2` FOREIGN KEY (`UsuarioID`) REFERENCES `usuario` (`ID`),
-  CONSTRAINT `categoria_ibfk_3` FOREIGN KEY (`TipoCategoriaID`) REFERENCES `tipo_categoria` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  KEY `grupo_UsuarioID_fk` (`UsuarioID`),
+  KEY `grupo_PastaID_fk` (`PastaID`),
+  KEY `grupo_TipoCategoriaID_fk` (`TipoCategoriaID`),
+  KEY `grupo_ArquivoID_fk` (`ArquivoID`),
+  CONSTRAINT `grupo_ArquivoID_fk` FOREIGN KEY (`ArquivoID`) REFERENCES `arquivos` (`ID`),
+  CONSTRAINT `grupo_PastaID_fk` FOREIGN KEY (`PastaID`) REFERENCES `pasta` (`ID`),
+  CONSTRAINT `grupo_TipoCategoriaID_fk` FOREIGN KEY (`TipoCategoriaID`) REFERENCES `tipo_categoria` (`ID`),
+  CONSTRAINT `grupo_UsuarioID_fk` FOREIGN KEY (`UsuarioID`) REFERENCES `usuario` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `planos`
+-- Table structure for table `pasta`
 --
 
-DROP TABLE IF EXISTS `planos`;
+DROP TABLE IF EXISTS `pasta`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
-CREATE TABLE `planos` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `Nome` varchar(255) DEFAULT NULL,
-  `Preco` varchar(255) DEFAULT NULL,
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pasta` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ArquivoID` int(11) DEFAULT NULL,
+  `UsuarioID` int(11) DEFAULT NULL,
+  `TipoCategoriaID` int(11) DEFAULT NULL,
+  `PermissaoID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `pasta` (`ArquivoID`),
+  KEY `pasta_UsuarioID_fk` (`UsuarioID`),
+  KEY `pasta_TipoCategoriaID_fk` (`TipoCategoriaID`),
+  KEY `pasta_PermissaoID_fk` (`PermissaoID`),
+  CONSTRAINT `pasta` FOREIGN KEY (`ArquivoID`) REFERENCES `arquivos` (`ID`),
+  CONSTRAINT `pasta_PermissaoID_fk` FOREIGN KEY (`PermissaoID`) REFERENCES `permissao` (`ID`),
+  CONSTRAINT `pasta_TipoCategoriaID_fk` FOREIGN KEY (`TipoCategoriaID`) REFERENCES `tipo_categoria` (`ID`),
+  CONSTRAINT `pasta_UsuarioID_fk` FOREIGN KEY (`UsuarioID`) REFERENCES `usuario` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `permissao`
+--
+
+DROP TABLE IF EXISTS `permissao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `permissao` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `UsuarioID` int(11) DEFAULT NULL,
+  `eAdministrador` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -79,13 +107,13 @@ CREATE TABLE `planos` (
 
 DROP TABLE IF EXISTS `tipo_categoria`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tipo_categoria` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `Indice` int DEFAULT NULL,
-  `Descricao` varchar(255) DEFAULT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Indice` int(11) DEFAULT NULL,
+  `Descricao` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -94,51 +122,14 @@ CREATE TABLE `tipo_categoria` (
 
 DROP TABLE IF EXISTS `usuario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `usuario` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `Nome` varchar(255) DEFAULT NULL,
-  `Email` varchar(255) DEFAULT NULL,
-  `Senha` varchar(255) DEFAULT NULL,
-  `Token` varchar(255) DEFAULT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Nome` varchar(50) DEFAULT NULL,
+  `Email` varchar(50) DEFAULT NULL,
+  `Senha` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `vinculo_planos_usuario`
---
-
-DROP TABLE IF EXISTS `vinculo_planos_usuario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
-CREATE TABLE `vinculo_planos_usuario` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `UsuarioID` int DEFAULT NULL,
-  `PlanosID` int DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `UsuarioID` (`UsuarioID`),
-  KEY `PlanosID` (`PlanosID`),
-  CONSTRAINT `vinculo_planos_usuario_ibfk_1` FOREIGN KEY (`UsuarioID`) REFERENCES `usuario` (`ID`),
-  CONSTRAINT `vinculo_planos_usuario_ibfk_2` FOREIGN KEY (`PlanosID`) REFERENCES `planos` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `vinculo_usuario_pagamento`
---
-
-DROP TABLE IF EXISTS `vinculo_usuario_pagamento`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
-CREATE TABLE `vinculo_usuario_pagamento` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `VinculoPlanoUsuarioID` int DEFAULT NULL,
-  `ApiToken` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `VinculoPlanoUsuarioID` (`VinculoPlanoUsuarioID`),
-  CONSTRAINT `vinculo_usuario_pagamento_ibfk_1` FOREIGN KEY (`VinculoPlanoUsuarioID`) REFERENCES `vinculo_planos_usuario` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -150,4 +141,4 @@ CREATE TABLE `vinculo_usuario_pagamento` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-08-14 20:32:50
+-- Dump completed on 2020-10-08 15:07:58
