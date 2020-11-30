9c39d6f4636a6058efdf9bac805da08b6f0ea5cd
Victor Manduca <30962377+VictorManduca@users.noreply.github.com>
2020-11-30 19:26:57 -0300

    main branch (#11)
    Co-authored-by: Brandon-Nitopi <brandon.nitopi@hotmail.com>

---
+++ client/assets/scripts/apresentation.js
@@ -0,0 +1,4 @@
+export function apresentation() {
+      this.$toast.success('Register')
+      this.$router.push({name: 'new_account'})
+}
--- client/pages/welcome.vue
+++ client/pages/welcome.vue
@@ -1,53 +1,5 @@
-<template>
-    <form class="form_welcome" action="">
-          <div class="card">
-              <div class="card-top">
-                <h1 class="titulo">Welcome to DevDrop</h1><br><br>
-                <img class="img_login" src="img/blue box.png" alt="">
-                <h3>Saveguard your life here!</h3>
-                <h3>Saveguard your data!</h3>
-                <h3>Stay safe!</h3>
-              </div>
-
-          </div>
-      </form>
-
-</template>
-
-<script>
-export default {};
-</script>
+<template src="../assets/templates/welcome.html"></template>
 
 <style>
-body {
-  background-color: rgb(76, 76, 196);
-  font-family: sans-serif;
-}
-
-.form_welcome {
-    width: 400px;
-    margin: auto;
-    padding-top: 40px;
-}
-
-.card-top {
-  text-align: center;
-}
-
-.card {
-  box-shadow: #cccccc;
-  background-color: cornsilk;
-  padding: 30px;
-  border-radius: 5px;
-}
-
-.titulo {
-  color: darkgrey;
-}
-
-.img_login {
-    border-radius: 10px;
-    width: 80px;
-    box-shadow: 1px 1px 5px #ccc;
-}
+@import '../assets/styles/welcome.css';
 </style>
