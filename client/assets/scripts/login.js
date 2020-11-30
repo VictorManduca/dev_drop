efe95e27953a538cbda4749bb2202a76146e3c87
Brandon-Nitopi <brandon.nitopi@hotmail.com>
2020-11-30 18:41:03 -0300

    main branch

--- client/assets/scripts/login.js
+++ client/assets/scripts/login.js
@@ -4,13 +4,16 @@ export async function login() {
 	try {
     const responseApi = await callLoginApi({ email: this.email, password: this.password })
 
-    if (responseApi.status == 202) {
+    if (responseApi.status == 202) { //status 200
       this.$toast.success('Successfully authenticated')
-      this.$router.push('http://google.com.br')
+      this.$router.push({name: 'welcome'})
     } else {
       this.$toast.error('Something is wrong with your email or password')
     }
   } catch (error) {
     return Promise.reject(`[scripts|login] ${ error }`)
   }
+
+
+
 }
--- client/pages/apresentation.vue
+++ client/pages/apresentation.vue
@@ -1,79 +1,21 @@
-<template>
-    <form class="form_in_up" action="">
-        <h1>DevDrop</h1><br>
-        <h3>Create your DevDrop account now</h3><br>
-        <button class="registro_button" type="submit">Register for free</button><br>
-        <h3>Already have account? Login</h3>
-
-        <form class="form_apresentation" action="">
-         <div class="card_apresentation">
-              <div class="card-top">
-                <h1 class="titulo">join the DevDrop community</h1><br><br>
-                <img class="img_apresentation" src="img/blue box.png" alt="">
-              </div>
-
-          </div>
-        </form>
-      </form>
-
-</template>
-
-<script>
-export default {};
-</script>
+<template src="../assets/templates/apresentation.html"></template>
 
 <style>
-body {
-  background-color: rgb(76, 76, 196);
-  font-family: sans-serif;
-}
-
-.card-top {
-  text-align: center;
-}
-
-.card_apresentation {
-    background-image:linear-gradient(to right, rgb(63, 63, 187), darkslateblue);
-    box-shadow: #cccccc;
-    background-color: cornsilk;
-    padding: 30px;
-    border-radius: 5px;
-}
-
-.form_in_up {
-    background-image:linear-gradient(to right, darkslateblue, rgb(63, 63, 187));
-    width: 400px;
-    display: inline-block;
-    margin-left: 400px;
-    padding-top: 6px;
-    text-emphasis-color: white;
-    border-radius: 20px;
-    text-align: center;
-}
-
-.form_apresentation {
-    width: 400px;
-    display: inline-block;
-    margin-left: 10px;
-    margin-top: 90px;
-    padding-top: 40px;
-}
-
-.img_apresentation {
-    background-color: #ccc;
-    border-radius: 20px;
-    width: 150px;
-    box-shadow: 1px 1px 5px #ccc;
-}
-
-.registro_button {
-    background-image:linear-gradient(to right, rgb(63, 63, 187), rgb(43, 14, 231));
-    width: 340px;
-    border-radius: 20px;
-    padding: 10px;
-    color: white;
-    border: 0px;
-    outline: 0px;
-}
-    
+@import '../assets/styles/apresentation.css';
 </style>
+
+<!--<script>
+import { apresentation } from '../assets/scripts/apresentation'
+import { callApresentationApi } from '../assets/scripts/api/call-apresentation'
+
+export default {
+	data: () => {
+		return {
+		}
+	},
+	methods: {
+		apresentation,
+		callApresentationApi
+	}
+}
+</script>-->
