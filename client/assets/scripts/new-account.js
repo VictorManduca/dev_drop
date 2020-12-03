008f9b73753db6fafa49d5dd82f57667074816ee
victormanduca <victor.manduca@helpper.com.br>
2020-12-03 20:24:18 -0300

    Fix new account

--- client/assets/scripts/new-account.js
+++ client/assets/scripts/new-account.js
@@ -1,21 +1,26 @@
 import { callNewAccountApi } from './api/call-new-account'
 
 export async function newAccount() {
-	try {
+  try {
     if (this.email == null || this.pass == null || this.name == null) {
-      this.$toast.error('Something went wrong')
+      return this.$toast.error('Blank inputs')
     } else {
-      const responseApi = await callNewAccountApi({ email: this.email, senha: this.pass, nome: this.name })
+      const responseApi = await callNewAccountApi({
+        email: this.email,
+        senha: this.pass,
+        nome: this.name,
+      })
 
       if (responseApi.status == 201) {
-        this.$toast.success('Account created successfully')
-        this.$router.push({ name: 'home' })
+        await this.$toast.success('Account created successfully')
+        return await this.$router.push({ name: 'index' })
       } else {
-        this.$toast.error('Something went wrong')
+        return this.$toast.error('Something went wrong')
       }
     }
   } catch (error) {
-    return Promise.reject(`[scripts|newAccount] ${ error }`)
+    console.error({ error: `[scripts|newAccount] ${error}` })
+    return this.$toast.error('Something went wrong')
   }
 }
 
